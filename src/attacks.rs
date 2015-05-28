d166a1b54da9f71c95ff0f50aab70e1c9f53ec6f
Topher Winward <winwardo@gmail.com>
2015-05-28 23:03:01 +0100

    Move problem_6 attack out

--- src/attacks.rs
+++ src/attacks.rs
@@ -20,6 +20,98 @@ pub fn guess_single_xor_char_decode(bytes: &Vec<u8>) -> (u16, Vec<u8>, u8) {
 	(top_score, top_decode, top_x)
 }
 
+pub fn guess_repeating_xor_key(data_bytes: &Vec<u8>, max_keysize: u16) -> (u16, Vec<u8>, Vec<u8>) {
+	use byte_manipulation::*;
+	use byte_utilities::*;
+	use english_scoring::*;
+	use general_utilities::*;
+	use key::*;
+
+	// For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes,
+	// and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
+	// The KEYSIZE with the smallest normalized edit distance is probably the key.
+	//  You could proceed perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
+	let mut keysize_points = Vec::new();
+	for keysize in 2..41 {
+		if data_bytes.len() < keysize * 4 {
+			continue;
+		}
+
+		let chunks: Vec<Vec<u8>> = data_bytes.chunks(keysize).map(|x| slice_to_vec(x)).collect();
+		assert!(chunks.len() >= 4);
+
+		let distance1 = hamming_distance(&chunks[0], &chunks[1]);
+		let distance2 = hamming_distance(&chunks[1], &chunks[2]);
+		let distance3 = hamming_distance(&chunks[2], &chunks[3]);
+		let distance4 = hamming_distance(&chunks[3], &chunks[4]);
+		let avg_distance: f32 = (distance1 + distance2 + distance3 + distance4) as f32 / 4.0;
+		let normalized_distance = avg_distance / keysize as f32;
+
+		let pair = (keysize, normalized_distance);
+		keysize_points.push(pair);
+	}
+
+	// Find the 4 best keysize values
+	keysize_points.sort_by(|a, b| {
+		use std::cmp::Ordering::*;
+
+		let (_, x): (_, f32) = *a;
+		let (_, y): (_, f32) = *b;
+
+		// f32s require partial cmp
+		x.partial_cmp(&y).unwrap_or(Equal)
+	});
+
+	//println!("{:?}", keysize_points);
+
+	let mut top_score = 0;
+	let mut top_decode: Vec<u8> = Vec::new();
+	let mut top_key: Vec<u8> = Vec::new();
+
+	for y in 0..10 {
+		let (best_keysize, _) = keysize_points[11-y];
+		//println!("{:?}", best_keysize);
+
+		// Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
+		let blocks: Vec<Vec<u8>> = data_bytes
+			.chunks(best_keysize)
+			.map(|x| slice_to_vec(x))
+			.collect();
+
+		// Now transpose the blocks: make a block that is the first byte of every block, and a block that is the second byte of every block, and so on.
+		let transposed_blocks: Vec<Vec<u8>> = transpose_chunks(&blocks);
+
+		// Solve each block as if it was single-character XOR. You already have code to do this.
+		//: Vec<(u16, String, u8)>
+		let solved_key: Vec<_> = transposed_blocks.iter()
+			.map(|x| {
+				let (_, _, guess) = guess_single_xor_char_decode(x);
+				guess
+			})
+			.collect();
+
+		// For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block.
+		// Put them together and you have the key.
+		//let final_blocks: Vec<Vec<u8>> = transpose_chunks(&solved_blocks);
+
+		//println!("{:?}, {:?}", solved_key, bytes_to_readable_text(&solved_key));
+
+		let mut rk = RepeatingKey::new_bytes(&solved_key);
+		let dec = rk.encrypt_bytes(&data_bytes);
+
+		//println!("\n\n{}\n\n{:?}", 11-y, bytes_to_readable_text(&dec));
+
+		let score = score_combined(&dec);
+		if score > top_score {
+			top_score = score;
+			top_decode = dec;
+			top_key = solved_key;
+		}
+	}
+
+	(top_score, top_decode, top_key)
+}
+
 //-----------------------------------------------------------------------------
 
 #[cfg(test)]
--- src/main__.rs
+++ src/main__.rs
@@ -43,98 +43,12 @@ fn problem_4() {
 
 fn problem_6() {
 	// http://cryptopals.com/sets/1/challenges/6/
-
 	use byte_conversion::*;
-	use byte_manipulation::*;
-	use byte_utilities::*;
-	use english_scoring::*;
-	use general_utilities::*;
-	use key::*;
 
 	let file_data = general_utilities::read_file("C:\\Users\\Topher\\Dropbox\\Public\\Programming\\Matasano\\matasano-crypto-challenges\\res\\6.txt");
 	let data_bytes = base64_to_bytes(&file_data);
 
-	// For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes,
-	// and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
-	// The KEYSIZE with the smallest normalized edit distance is probably the key.
-	//  You could proceed perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
-	let mut keysize_points = Vec::new();
-	for keysize in 2..41 {
-		if data_bytes.len() < keysize * 4 {
-			continue;
-		}
-
-		let chunks: Vec<Vec<u8>> = data_bytes.chunks(keysize).map(|x| slice_to_vec(x)).collect();
-		assert!(chunks.len() >= 4);
-
-		let distance1 = hamming_distance(&chunks[0], &chunks[1]);
-		let distance2 = hamming_distance(&chunks[1], &chunks[2]);
-		let distance3 = hamming_distance(&chunks[2], &chunks[3]);
-		let distance4 = hamming_distance(&chunks[3], &chunks[4]);
-		let avg_distance: f32 = (distance1 + distance2 + distance3 + distance4) as f32 / 4.0;
-		let normalized_distance = avg_distance / keysize as f32;
-
-		let pair = (keysize, normalized_distance);
-		keysize_points.push(pair);
-	}
-
-	// Find the 4 best keysize values
-	keysize_points.sort_by(|a, b| {
-		use std::cmp::Ordering::*;
-
-		let (_, x): (_, f32) = *a;
-		let (_, y): (_, f32) = *b;
-
-		// f32s require partial cmp
-		x.partial_cmp(&y).unwrap_or(Equal)
-	});
-
-	//println!("{:?}", keysize_points);
-
-	let mut top_score = 0;
-	let mut top_decode: Vec<u8> = Vec::new();
-	let mut top_key: Vec<u8> = Vec::new();
-
-	for y in 0..10 {
-		let (best_keysize, _) = keysize_points[11-y];
-		//println!("{:?}", best_keysize);
-
-		// Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
-		let blocks: Vec<Vec<u8>> = data_bytes
-			.chunks(best_keysize)
-			.map(|x| slice_to_vec(x))
-			.collect();
-
-		// Now transpose the blocks: make a block that is the first byte of every block, and a block that is the second byte of every block, and so on.
-		let transposed_blocks: Vec<Vec<u8>> = transpose_chunks(&blocks);
-
-		// Solve each block as if it was single-character XOR. You already have code to do this.
-		//: Vec<(u16, String, u8)>
-		let solved_key: Vec<_> = transposed_blocks.iter()
-			.map(|x| {
-				let (_, _, guess) = attacks::guess_single_xor_char_decode(x);
-				guess
-			})
-			.collect();
-
-		// For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block.
-		// Put them together and you have the key.
-		//let final_blocks: Vec<Vec<u8>> = transpose_chunks(&solved_blocks);
-
-		//println!("{:?}, {:?}", solved_key, bytes_to_readable_text(&solved_key));
-
-		let mut rk = RepeatingKey::new_bytes(&solved_key);
-		let dec = rk.encrypt_bytes(&data_bytes);
-
-		//println!("\n\n{}\n\n{:?}", 11-y, bytes_to_readable_text(&dec));
-
-		let score = english_scoring::score_combined(&dec);
-		if score > top_score {
-			top_score = score;
-			top_decode = dec;
-			top_key = solved_key;
-		}
-	}
+	let (top_score, top_decode, top_key) = attacks::guess_repeating_xor_key(&data_bytes, 40);
 
 	println!("{}, {}, \n{:?}", bytes_to_readable_text(&top_key), top_score, bytes_to_readable_text(&top_decode));
 }

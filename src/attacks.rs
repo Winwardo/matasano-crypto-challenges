e46e7a3d8b51029ded8488d4e0c8f0d0e92197e8
Topher Winward <winwardo@gmail.com>
2015-05-28 23:18:37 +0100

    Adding repeating_xor_tests trivial

--- src/attacks.rs
+++ src/attacks.rs
@@ -20,19 +20,26 @@ pub fn guess_single_xor_char_decode(bytes: &Vec<u8>) -> (u16, Vec<u8>, u8) {
 	(top_score, top_decode, top_x)
 }
 
-pub fn guess_repeating_xor_key(data_bytes: &Vec<u8>, max_keysize: u16) -> (u16, Vec<u8>, Vec<u8>) {
+pub fn guess_repeating_xor_key(data_bytes: &Vec<u8>, max_keysize: usize) -> (u16, Vec<u8>, Vec<u8>) {
+	// As described at http://cryptopals.com/sets/1/challenges/6/
 	use byte_manipulation::*;
 	use byte_utilities::*;
 	use english_scoring::*;
 	use general_utilities::*;
 	use key::*;
 
+	// Shortcut single character key guesses
+	if (max_keysize == 1) {
+		let (a, b, c) = guess_single_xor_char_decode(&data_bytes);
+		return (a, b, vec![c]);
+	}
+
 	// For each KEYSIZE, take the first KEYSIZE worth of bytes, and the second KEYSIZE worth of bytes,
 	// and find the edit distance between them. Normalize this result by dividing by KEYSIZE.
 	// The KEYSIZE with the smallest normalized edit distance is probably the key.
 	//  You could proceed perhaps with the smallest 2-3 KEYSIZE values. Or take 4 KEYSIZE blocks instead of 2 and average the distances.
 	let mut keysize_points = Vec::new();
-	for keysize in 2..41 {
+	for keysize in 2..max_keysize+1 {
 		if data_bytes.len() < keysize * 4 {
 			continue;
 		}
@@ -51,7 +58,7 @@ pub fn guess_repeating_xor_key(data_bytes: &Vec<u8>, max_keysize: u16) -> (u16, 
 		keysize_points.push(pair);
 	}
 
-	// Find the 4 best keysize values
+	// Sort into the best keysize values
 	keysize_points.sort_by(|a, b| {
 		use std::cmp::Ordering::*;
 
@@ -62,15 +69,16 @@ pub fn guess_repeating_xor_key(data_bytes: &Vec<u8>, max_keysize: u16) -> (u16, 
 		x.partial_cmp(&y).unwrap_or(Equal)
 	});
 
-	//println!("{:?}", keysize_points);
-
 	let mut top_score = 0;
 	let mut top_decode: Vec<u8> = Vec::new();
 	let mut top_key: Vec<u8> = Vec::new();
 
+	// Check the best 10, any more than that starts using up a lot of time.
 	for y in 0..10 {
-		let (best_keysize, _) = keysize_points[11-y];
-		//println!("{:?}", best_keysize);
+		if y >= keysize_points.len() {
+			break;
+		}
+		let (best_keysize, _) = keysize_points[y];
 
 		// Now that you probably know the KEYSIZE: break the ciphertext into blocks of KEYSIZE length.
 		let blocks: Vec<Vec<u8>> = data_bytes
@@ -92,19 +100,13 @@ pub fn guess_repeating_xor_key(data_bytes: &Vec<u8>, max_keysize: u16) -> (u16, 
 
 		// For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block.
 		// Put them together and you have the key.
-		//let final_blocks: Vec<Vec<u8>> = transpose_chunks(&solved_blocks);
+		let mut repeating_key = RepeatingKey::new_bytes(&solved_key);
+		let decoded = repeating_key.encrypt_bytes(&data_bytes);
 
-		//println!("{:?}, {:?}", solved_key, bytes_to_readable_text(&solved_key));
-
-		let mut rk = RepeatingKey::new_bytes(&solved_key);
-		let dec = rk.encrypt_bytes(&data_bytes);
-
-		//println!("\n\n{}\n\n{:?}", 11-y, bytes_to_readable_text(&dec));
-
-		let score = score_combined(&dec);
+		let score = score_combined(&decoded);
 		if score > top_score {
 			top_score = score;
-			top_decode = dec;
+			top_decode = decoded;
 			top_key = solved_key;
 		}
 	}
@@ -147,6 +149,24 @@ mod test {
 
 		assert_eq!(example, guessed_decode);
 		assert_eq!('Q' as u8, guessed_character);
+		assert!(top_score > 0);
+	}
+
+	#[test]
+	fn guess_repeating_xor_key_decode_i_single_character() {
+		// http://cryptopals.com/sets/1/challenges/3/
+		use byte_conversion::*;
+		use key::*;
+
+		let example_key: Vec<u8> = readable_text_to_bytes(&"Q");
+		let mut rk = RepeatingKey::new_bytes(&example_key);
+		let example = readable_text_to_bytes(&"Some super hard to decrypt (but still English!) text.");
+
+		let encrypted = rk.encrypt_bytes(&example);
+		let (top_score, guessed_decode, guessed_key) = guess_repeating_xor_key(&encrypted, 1);
+
+		assert_eq!(example, guessed_decode);
+		assert_eq!(example_key, guessed_key);
 		assert!(top_score > 0);
 	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod byte_conversion;
 mod byte_manipulation;
 mod byte_utilities;
 mod english_scoring;
+mod general_utilities;
 mod key;
 
 fn main() {

62f104b0803815c6e5f6730db8ee020f714b509e
Topher Winward <winwardo@gmail.com>
2015-05-28 23:30:46 +0100

    Attacks non-trivial tests

--- src/attacks.rs
+++ src/attacks.rs
@@ -137,7 +137,6 @@ mod test {
 
 	#[test]
 	fn guess_single_xor_char_decode_i_example() {
-		// http://cryptopals.com/sets/1/challenges/3/
 		use byte_conversion::*;
 		use key::*;
 
@@ -154,7 +153,6 @@ mod test {
 
 	#[test]
 	fn guess_repeating_xor_key_decode_i_single_character() {
-		// http://cryptopals.com/sets/1/challenges/3/
 		use byte_conversion::*;
 		use key::*;
 
@@ -166,6 +164,42 @@ mod test {
 		let (top_score, guessed_decode, guessed_key) = guess_repeating_xor_key(&encrypted, 1);
 
 		assert_eq!(example, guessed_decode);
+		assert_eq!(example_key, guessed_key);
+		assert!(top_score > 0);
+	}
+
+	#[test]
+	fn guess_repeating_xor_key_decode_i_two_characters() {
+		use byte_conversion::*;
+		use key::*;
+
+		let example_key: Vec<u8> = readable_text_to_bytes(&"Qr");
+		let mut rk = RepeatingKey::new_bytes(&example_key);
+		let example = readable_text_to_bytes(&"Some super hard to decrypt (but still English!) text.");
+
+		let encrypted = rk.encrypt_bytes(&example);
+		let (top_score, guessed_decode, guessed_key) = guess_repeating_xor_key(&encrypted, 2);
+
+		assert_eq!(example, guessed_decode);
+		assert_eq!(example_key, guessed_key);
+		assert!(top_score > 0);
+	}
+
+
+	#[test]
+	fn guess_repeating_xor_key_decode_i_non_trivial_key() {
+		// http://cryptopals.com/sets/1/challenges/6/
+		use byte_conversion::*;
+		use key::*;
+
+		let example_key: Vec<u8> = readable_text_to_bytes(&"Terminator X: Bring the noise");
+		let mut rk = RepeatingKey::new_bytes(&example_key);
+		let example = readable_text_to_bytes(&"I\'m back and I\'m ringin\' the bell \nA rockin\' on the mike while the fly girls yell \nIn ecstasy in the back of me \nWell that\'s my DJ Deshay cuttin\' all them Z\'s \nHittin\' hard and the girlies goin\' crazy \nVanilla\'s on the mike, man I\'m not lazy. \n\nI\'m lettin\' my drug kick in \nIt controls my mouth and I begin \nTo just let it flow, let my concepts go \nMy posse\'s to the side yellin\', Go Vanilla Go! \n\nSmooth \'cause that\'s the way I will be \nAnd if youdon\'t give a damn, then \nWhy you starin\' at me \nSo get off \'cause I control the stage \nThere\'s no dissin\' allowed \nI\'m in my own phase \nThe girlies sa y they love me and that is ok \nAnd I can dance better than any kid n\' play \n\nStage 2 -- Yea the one ya\' wanna listen to \nIt\'s off my head so let the beat play through \nSo I can funk it up and make it sound good \n1-2-3 Yo -- Knock on some wood \nFor good luck, I like my rhymes atrocious \nSupercalafragilisticexpialidocious \nI\'m an effect and that you can bet \nI can take a fly girl and make her wet. \n\nI\'m like Samson -- Samson to Delilah \nThere\'s no denyin\', You can try to hang \nBut you\'ll keep tryin\' to get my style \nOver and over, practice makes perfect \nBut not if you\'re a loafer. \n\nYou\'ll get nowhere, no place, no time, no girls \nSoon -- Oh my God, homebody, you probably eat \nSpaghetti with a spoon! Come on and say it! \n\nVIP. Vanilla Ice yep, yep, I\'m comin\' hard like a rhino \nIntoxicating so you stagger like a wino \nSo punks stop trying and girl stop cryin\' \nVanilla Ice is sellin\' and you people are buyin\' \n\'Cause why the freaks are jockin\' like Crazy Glue \nMovin\' and groovin\' trying to sing along \nAll through the ghetto groovin\' this here song \nNow you\'re amazed by the VIP posse. \n\nSteppin\' so hard like a German Nazi \nStartled by the bases hittin\' ground \nThere\'s no trippin\' on mine, I\'m just gettin\' down \nSparkamatic, I\'m hangin\' tight like a fanatic \nYou trapped me once and I thought that \nYou might have it \nSo step down and lend me your ear \n\'89 in my time! You, \'90 is my year. \n\nYou\'re weakenin\' fast, YO! and I can tell it \nYour body\'s gettin\' hot, so, so I can smell it \nSo don\'t be mad and don\'t be sad \n\'Cause the lyrics belong to ICE, You can call me Dad \nYou\'re pitchin\' a fit, so step back and endure \nLet the witch doctor, Ice, do thedance to cure \nSo come up close and don\'t be square \nYou wanna battle me -- Anytime, anywhere \n\nYou thought that Iwas weak, Boy, you\'re dead wrong \nSo come on, everybody and sing this song \n\nSay -- Play that funky music Say, go white boy, go white boy go \nplay that funky music Go white boy, go white boy, go \nLay down and boogie and play that funky music till you die. \n\nPlay that funky music Come on, Come on, let me hear \nPlay that funky music white boy you sayit, say it \nPlay that funky music A little louder now \nPlay that funky music, white boy Come on, Come on, Come on \nPlay that funky music \n");
+
+		let encrypted = rk.encrypt_bytes(&example);
+		let (top_score, guessed_decode, guessed_key) = guess_repeating_xor_key(&encrypted, 29);
+
+		assert_eq!(bytes_to_readable_text(&example), bytes_to_readable_text(&guessed_decode));
 		assert_eq!(example_key, guessed_key);
 		assert!(top_score > 0);
 	}

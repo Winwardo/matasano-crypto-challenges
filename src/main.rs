33a6b67f0f3b629871bb6f4c4b67c78d8fd5622d
Topher Winward <winwardo@gmail.com>
2015-05-30 16:09:18 +0100

    Set 1 Challenge 8 complete

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ mod english_scoring;
 mod general_utilities;
 mod key;
 
+use std::collections::HashMap;
+
 fn problem_3() {
 	// http://cryptopals.com/sets/1/challenges/3/
 	use byte_conversion::*;
@@ -75,11 +77,54 @@ fn problem_7() {
     }
 }
 
+fn detect_ecb(ciphertext: &[u8]) -> bool {
+	false
+}
+
+fn problem_8() {
+	// http://cryptopals.com/sets/1/challenges/8/
+	use byte_conversion::*;
+
+	let file_data = general_utilities::read_file("C:\\Users\\Topher\\Dropbox\\Public\\Programming\\Matasano\\matasano-crypto-challenges\\res\\8.txt");
+	
+	// Now split into a vector of bytes
+	let bytes: Vec<Vec<u8>> = file_data
+		.split("\n")
+		.map(|x| hex_to_bytes(x))
+		.collect();
+
+	let key_size = 16;
+	let mut blocks_map: HashMap<&[u8], u8> = HashMap::new();
+	let mut ecb_candidates: Vec<Vec<u8>> = Vec::new();
+
+	for ciphertext in &bytes {
+		for block in ciphertext.chunks(key_size) {
+			let c = blocks_map.entry(&block).or_insert(0);
+			*c += 1;
+
+			if *c == 2 {
+				ecb_candidates.push(general_utilities::slice_to_vec(ciphertext));
+			}
+		}
+	}
+
+	for (k, v) in blocks_map {
+		if v > 1 {
+			println!("{:?}, {:?}, {:?}", v, k, bytes_to_hex(&general_utilities::slice_to_vec(k)));
+		}
+	}
+
+	println!("{:?}", ecb_candidates);
+
+
+	//println!("{:?}, {} {} {}", bytes, bytes[0].len(), bytes[1].len(), bytes[2].len());
+}
+
 
 fn main() {
-	println!("\n\n=====\nRunning.\n\n");
+	println!("=====\nRunning.\n");
 
-	problem_7();
+	problem_8();
 
 	println!("\nComplete.");
 }

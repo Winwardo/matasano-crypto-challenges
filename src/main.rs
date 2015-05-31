aa546b64dd2aadd999358f09c67e8b31643b8099
Topher Winward <winwardo@gmail.com>
2015-05-31 18:08:11 +0100

    Problem 9 setup

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,18 @@ mod attacks;
 mod byte_conversion;
 mod byte_manipulation;
 mod byte_utilities;
+mod ciphertext;
 mod english_scoring;
 mod general_utilities;
 mod key;
 
 mod set1;
+mod set2;
 
 fn main() {
 	println!("=====\nRunning.\n");
 
-	set1::set1();
+	set2::problem_9();
 
 	println!("\nComplete.");
 }
---
+++ src/set2.rs
@@ -0,0 +1,12 @@
+use attacks::*;
+use aes::*;
+use byte_conversion::*;
+use general_utilities::*;
+
+pub fn problem_9() {
+	// http://cryptopals.com/sets/2/challenges/9
+}
+
+pub fn set1() {
+	problem_9();
+}

d895a10dea5d8fa8ba263481316e381c4d0c1e7d
Topher Winward <hi@topher.io>
2015-08-11 22:56:47 +0100

    CBC decryption

--- src/cbc.rs
+++ src/cbc.rs
@@ -18,6 +18,14 @@ impl CipherBlock {
 
 		cipher.unwrap()
 	}
+	
+	pub fn to_plaintext(&mut self) -> Vec<u8> {
+		let key = self.key.of_length(16);
+		let decipher = decrypt_aes_128_ecb_no_padding(self.input.bytes(), &key).unwrap();
+		let xored = xor_byte_streams(&self.initialisation_vector, &decipher);
+		
+		xored
+	}
 }
 
 //-----------------------------------------------------------------------------
@@ -40,5 +48,28 @@ mod test {
 		};
 
 		assert_eq!(16, cipher_block.to_ciphertext().len());
+	}
+	
+	#[test]
+	fn RepeatingKey_encrypt_decrypt_equal() {
+		fn plaintext() -> PaddedBytes { PaddedBytes::from_text("Some text", 16).unwrap() };
+		fn IV() -> Vec<u8> { RepeatingKey::new(&"\x00").of_length(16) };
+		fn key() -> RepeatingKey { RepeatingKey::new(&"YELLOW SUBMARINE") };
+	
+		let mut cipher_block = CipherBlock {
+			input: plaintext(),
+			initialisation_vector: IV(),
+			key: key(),
+		};
+
+		let ciphertext = cipher_block.to_ciphertext();
+		
+		let mut decrypt_cipher = CipherBlock {
+			input: PaddedBytes::from_bytes(&ciphertext[..], 16).unwrap(),
+			initialisation_vector: IV(),
+			key: key(),
+		};
+		
+		assert_eq!(plaintext().vec(), &decrypt_cipher.to_plaintext());
 	}
 }
